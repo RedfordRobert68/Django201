83ea853b6e2daf2b58f783ad12bb6987be726cd1
Robert Vincent <redfordrobert68@gmail.com>
2023-02-21 13:10:16 -0500

    Updates to Til

--- til/settings.py
+++ til/settings.py
@@ -17,6 +17,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = "/media/"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -42,9 +45,11 @@ INSTALLED_APPS = [
     'django.contrib.sites',
 
     'feed',
+    'profiles',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'sorl.thumbnail'
 ]
 
 MIDDLEWARE = [
